d99d5a84c994029ffd4ba8b6e718ac66560b3f67
unknown <mhda49600@gmail.com>
2024-09-02 21:26:06 +0700

    end

--- Exercise.ts
+++ Exercise.ts
@@ -12,16 +12,53 @@ if(bilangan % 2 == 0) {
 // Soal 2
 // Memeriksa apakah bilangan Prima
 
-let cekBilangan = 10; 
+let cekBilangan = 13; 
 
-if(cekBilangan <= 1 ) {
-    console.log(`${cekBilangan} bukan bilangan Prima`);
+if(cekBilangan === 2  ) {
+    console.log(`${cekBilangan} adalah bilangan Prima`);
+} else if (cekBilangan === 1) {
+    console.log(`${cekBilangan} bukan bilangan prima`)
+} else if (cekBilangan % 2 !== 0) {
+    console.log(`${cekBilangan} adalah bilangan Prima`)
 } else {
-    for(let i = 2; i * i <= cekBilangan; i++) {
-        if(cekBilangan % i == 0) {
-            console.log(`${cekBilangan} bukan bilangan prima`)
-        } else {
-            console.log(`${cekBilangan} adalah bilangan prima`)
-        }
-    }
+    console.log(`${cekBilangan} bukan bilangan prima`)
 }
+
+// Soal 3 
+// bilangan 3 = 1 + 2 + 3 
+
+let N = 5;
+let total = 0; 
+ 
+for (let i = 0; i <= N; i++) { 
+   total = total + i; 
+}
+
+console.log(total);
+
+// Soal 4 
+// Faktorial 
+
+const Nfaktorial = 3;
+
+let result = 1;
+
+for (let i = Nfaktorial; i > 0; i--) {
+    result *= i;  
+}
+
+console.log(result);
+
+// Soal 5 
+// Fibonacci 
+
+let NFibonacci = 15;  
+let a = 0, b = 1;
+
+for (let i = 1; i < NFibonacci; i++) {
+    let temp = a;
+    a = b;
+    b = temp + b;
+}
+
+console.log(b);
